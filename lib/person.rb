35ad22d79f00de324161c35f77b1b55149f37ba3
Naya  Willis <greedybrain18@gmail.com>
2019-09-21 19:09:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,19 @@
 class Person
 
-  def name=(name)
-    @name = name
+  def name=(person_name)
+    @name = person_name
   end
   
   def name 
     @name
+  end
+  
+  def job=(job_title)
+    @job = job_title
+  end
+  
+  def job 
+    @job
   end
 
 end
